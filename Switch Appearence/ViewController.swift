bfcdd26ad073b91a92769cd40b330be251e61289
Joel Johnson <jjoel55@gmail.com>
2018-12-17 15:45:26 -0500

    Initial Commit

---
+++ Switch Appearence/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Switch Appearence
+//
+//  Created by Joel Johnson on 12/17/18.
+//  Copyright © 2018 Joel Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
